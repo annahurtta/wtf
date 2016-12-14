fdd6516ff245f880756f5786a77f9cb9166bf616
Anna Hurtta <anna.hurtta@yle.fi>
2016-12-14 13:32:33 +0200

    Muokattu hakutoimintoa oikealla datalla

--- js/script.js
+++ js/script.js
@@ -1,25 +1,35 @@
 var app = {
+  // getData: function(){
+  //   $.ajax({
+  //     url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=200&callback=?&q=' + encodeURIComponent('http://www.allflicks.fi/rss/rss.xml'),
+  //     dataType: 'json',
+  //     success: function (data) {
+  //       if (data.responseData.feed && data.responseData.feed.entries) {
+  //         data = data.responseData.feed.entries;
+  //       }
+  //     app.searchData(data);
+  //     }
+  //   });
+  // },
   getData: function(){
     $.ajax({
-      url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=200&callback=?&q=' + encodeURIComponent('http://www.allflicks.fi/rss/rss.xml'),
+      url: 'data/data.json',
       dataType: 'json',
       success: function (data) {
-        if (data.responseData.feed && data.responseData.feed.entries) {
-          data = data.responseData.feed.entries;
-        }
-      app.searchData(data);
+        console.log(data)
+        app.searchData(data);
       }
     });
   },
   searchData: function(data) {
     app.availableTags = [];
     $.each(data, function (key, value) {
-      app.availableTags.push(value.title);
+      app.availableTags.push(key);
     });
   
     $("#titles").autocomplete({
-        source: app.availableTags
-      });
+      source: app.availableTags
+    });
 
     $('.search_btn').click(function (){
       console.log('clicked')
@@ -35,15 +45,23 @@ var app = {
   },
   printInfo: function(data){
       var resultContainer = $('.result_container').empty();
-      var myName = app.selectedName;
+      var pickedMovie = app.selectedName;
       
-      console.log(myName)
-      data.map(function(movie){
-        if(myName === movie.title){
-          console.log(movie)
-          $(movie.content).appendTo(resultContainer)
+      console.log(pickedMovie)
+      $.each(data, function (key, value) {
+        if(pickedMovie === key){
+          console.log(value.FIELD3);
+          console.log(value.FIELD4);
+          console.log(value.FIELD5);
+          $(value.FIELD3).appendTo(resultContainer);
         }
-      })
+      });
+      // data.map(function(movie){
+      //   if(myName === movie.title){
+      //     console.log(movie)
+      //     $(movie.content).appendTo(resultContainer)
+      //   }
+      // })
   },
   init: function() {
     app.getData();
